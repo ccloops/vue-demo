e477a65311b2d948748e780fecc7bbdc81041678
Catherine <loopercat@gmail.com>
2019-01-09 09:24:26 -0800

    adding a form and form validation

--- main.js
+++ main.js
@@ -40,6 +40,18 @@ Vue.component('product', {
         <button @click="removeFromCart">Remove Item</button>
       </div>
 
+      <div>
+        <h2>Reviews</h2>
+        <p v-if="!reviews.length">There are no reviews yet.</p>
+        <ul>
+          <li v-for="review in reviews">
+            <p>{{ review.name }}</p>
+            <p>Rating: {{ review.rating }}</p>
+            <p>{{ review.review }}</p>
+          </li>
+        </ul>
+      </div>
+
       <product-review @review-submitted="addReview"></product-review>
     </div>
   `,
@@ -109,6 +121,14 @@ Vue.component('product', {
 Vue.component('product-review', {
   template: `
     <form class="review-form" @submit.prevent="onSubmit">
+
+      <p v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+        <ul>
+          <li v-for="error in errors">{{ error }}</li>
+        </ul>
+      </p>
+      
       <p>
         <label for="name">Name:</label>
         <input id="name" v-model="name" placeholder="name">
@@ -122,11 +142,11 @@ Vue.component('product-review', {
       <p>
         <label for="rating">Rating:</label>
         <select id="rating" v-model.number="rating">
-          <option>5<option>
-          <option>4<option>
-          <option>3<option>
-          <option>2<option>
-          <option>1<option>
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
         </select>
       </p>
 
@@ -138,21 +158,28 @@ Vue.component('product-review', {
   data() {
     return {
       name: null,
+      review: null,
       rating: null,
-      review: null
+      errors: []
     }
   },
   methods: {
     onSubmit() {
-      let productReview = {
-        name: this.name,
-        review: this.review,
-        rating: this.rating
+      if(this.name && this.review && this.rating) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating
+        }
+        this.$emit('review-submitted', productReview)
+        this.name = null
+        this.review = null
+        this.rating = null
+      } else {
+        if(!this.name) this.errors.push('Name required.')
+        if(!this.review) this.errors.push('Review required.')
+        if(!this.rating) this.errors.push('Rating required.')
       }
-      this.$emit('review-submitted', productReview)
-      this.name = null
-      this.review = null
-      this.rating = null
     }
   }
 })
